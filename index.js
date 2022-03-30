f5b207b2ada75afe029e8c3b14dcf77891792635
iamthefastone <o.osisiogu@gmail.com>
2022-03-30 14:24:34 +0100

    Program password generator Note to self: Explore more ways to shorten this code

---
+++ index.js
@@ -0,0 +1,78 @@
+let char = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0","1","2","3","4","5","6","7","8","9", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",";","'","<",",",">",".","?","/"]
+let resultOne = document.querySelector("#result-1")
+let resultTwo = document.querySelector("#result-2")
+let resultThree = document.querySelector("#result-3")
+let resultFour = document.querySelector("#result-4")
+
+// console.log(char.length)
+
+function generatePasswords()    {
+    generatePassOne();
+    generatePassTwo();
+    generatePassThree();
+    generatePassFour();
+}
+
+function generatePassOne() {
+    let charOne = char[Math.floor(Math.random() * char.length)]
+    let charTwo = char[Math.floor(Math.random() * char.length)]
+    let charThree = char[Math.floor(Math.random() * char.length)]
+    let charFour = char[Math.floor(Math.random() * char.length)]
+    let charFive = char[Math.floor(Math.random() * char.length)]
+    let charSix = char[Math.floor(Math.random() * char.length)]
+    let charSeven = char[Math.floor(Math.random() * char.length)]
+    let charEight = char[Math.floor(Math.random() * char.length)]
+    let charNine = char[Math.floor(Math.random() * char.length)]
+    let charTen = char[Math.floor(Math.random() * char.length)]
+    let charEleven = char[Math.floor(Math.random() * char.length)]
+    let charTwelve = char[Math.floor(Math.random() * char.length)]
+    resultOne.textContent = charOne+charTwo+charThree+charFour+charFive+charSix+charSeven+charEight+charNine+charTen+charEleven+charTwelve
+}
+
+function generatePassTwo() {
+    let charOne = char[Math.floor(Math.random() * char.length)]
+    let charTwo = char[Math.floor(Math.random() * char.length)]
+    let charThree = char[Math.floor(Math.random() * char.length)]
+    let charFour = char[Math.floor(Math.random() * char.length)]
+    let charFive = char[Math.floor(Math.random() * char.length)]
+    let charSix = char[Math.floor(Math.random() * char.length)]
+    let charSeven = char[Math.floor(Math.random() * char.length)]
+    let charEight = char[Math.floor(Math.random() * char.length)]
+    let charNine = char[Math.floor(Math.random() * char.length)]
+    let charTen = char[Math.floor(Math.random() * char.length)]
+    let charEleven = char[Math.floor(Math.random() * char.length)]
+    let charTwelve = char[Math.floor(Math.random() * char.length)]
+    resultTwo.textContent = charOne+charTwo+charThree+charFour+charFive+charSix+charSeven+charEight+charNine+charTen+charEleven+charTwelve
+}
+
+function generatePassThree() {
+    let charOne = char[Math.floor(Math.random() * char.length)]
+    let charTwo = char[Math.floor(Math.random() * char.length)]
+    let charThree = char[Math.floor(Math.random() * char.length)]
+    let charFour = char[Math.floor(Math.random() * char.length)]
+    let charFive = char[Math.floor(Math.random() * char.length)]
+    let charSix = char[Math.floor(Math.random() * char.length)]
+    let charSeven = char[Math.floor(Math.random() * char.length)]
+    let charEight = char[Math.floor(Math.random() * char.length)]
+    let charNine = char[Math.floor(Math.random() * char.length)]
+    let charTen = char[Math.floor(Math.random() * char.length)]
+    let charEleven = char[Math.floor(Math.random() * char.length)]
+    let charTwelve = char[Math.floor(Math.random() * char.length)]
+    resultThree.textContent = charOne+charTwo+charThree+charFour+charFive+charSix+charSeven+charEight+charNine+charTen+charEleven+charTwelve
+}
+
+function generatePassFour() {
+    let charOne = char[Math.floor(Math.random() * char.length)]
+    let charTwo = char[Math.floor(Math.random() * char.length)]
+    let charThree = char[Math.floor(Math.random() * char.length)]
+    let charFour = char[Math.floor(Math.random() * char.length)]
+    let charFive = char[Math.floor(Math.random() * char.length)]
+    let charSix = char[Math.floor(Math.random() * char.length)]
+    let charSeven = char[Math.floor(Math.random() * char.length)]
+    let charEight = char[Math.floor(Math.random() * char.length)]
+    let charNine = char[Math.floor(Math.random() * char.length)]
+    let charTen = char[Math.floor(Math.random() * char.length)]
+    let charEleven = char[Math.floor(Math.random() * char.length)]
+    let charTwelve = char[Math.floor(Math.random() * char.length)]
+    resultFour.textContent = charOne+charTwo+charThree+charFour+charFive+charSix+charSeven+charEight+charNine+charTen+charEleven+charTwelve
+}
